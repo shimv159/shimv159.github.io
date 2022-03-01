1c2b9adc630a0183c2214e28aa29ecea38e04e5b
vanessannx <shimv159@newschool.edu>
2022-03-01 11:44:38 -0500

    ass5

--- studio-2022/world-building/girlhood-and-music/script.js
+++ studio-2022/world-building/girlhood-and-music/script.js
@@ -1,12 +1,12 @@
-// let toggleMenu = document.querySelector(".mobile-menu");
+let toggleMenu = document.querySelector(".mobile-menu");
 
-// let headerStatus = document.querySelector(".header");
+let headerStatus = document.querySelector(".header");
 
-// toggleMenu.addEventListener("click",() => {
-// 	if (headerStatus.classList.contains("open")) {
-// 		headerStatus.classList.remove("open");
-// 	} else {
-// 		headerStatus.classList.add("open");
+toggleMenu.addEventListener("click",() => {
+	if (headerStatus.classList.contains("open")) {
+		headerStatus.classList.remove("open");
+	} else {
+		headerStatus.classList.add("open");
 
-// 	}
-// })
+	}
+})
