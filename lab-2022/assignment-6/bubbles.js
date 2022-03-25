404dc68bac6d3bc9a8b30123f5e08336bed6e79d
vanessannx <shimv159@newschool.edu>
2022-03-25 02:59:12 -0400

    ass6 final

--- lab-2022/assignment-6/bubbles.js
+++ lab-2022/assignment-6/bubbles.js
@@ -1,11 +1,6 @@
 let bubbles = document.querySelectorAll('.bubble');
 let container = document.querySelector('#container');
   
-// bubbles.forEach(function (bubble) {
-//   bubble.style.left = (90 * Math.random()) + '%';
-//   bubble.style.top = (90 * Math.random()) + '%';
-// });
-
 container.addEventListener('click', function (event) {
   if (event.target.classList.contains('bubble')) {
     event.target.remove();
